fcad79760e9f73a2d8636a7d3c8494690382d404
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-07-12 23:24:03 +0530

    remove backend directory

--- backend/src/routes/foodMenuRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createFoodItem } from "../controllers/BusinessControllers/foodMenuController.js";
-
-const router = express.Router();
-
-// Route to create a food item
-router.post("/", createFoodItem);
-
-export default router;
