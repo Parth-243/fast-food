e2695f8822f555bede40551d41d41d0fc309b817
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-07-15 03:01:26 +0530

    integrate the current user and food category apis and logout api

--- src/component/Layout/adminHeader.jsx
+++ src/component/Layout/adminHeader.jsx
@@ -1,8 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./restaurantHeader.css";
 
 const AdminHeader = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/admin/auth/user", {
+          method: "GET",
+          credentials: "include", // Ensure cookies are included in the request
+        });
+
+        if (response.status === 401) {
+          navigate("/adminLogin");
+          return;
+        }
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user details");
+        }
+
+        const userData = await response.json();
+        setUser(userData);
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+
+    fetchUser();
+  }, [navigate]);
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/admin/auth/logout", {
+        method: "POST",
+        credentials: "include", // Ensure cookies are included in the request
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to log out");
+      }
+
+      setUser(null);
+      navigate("/adminLogin"); // Redirect to the login page after logout
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
   return (
     <header className="header">
       <div className="header-container">
@@ -11,7 +58,15 @@ const AdminHeader = () => {
         </div>
         <nav className="nav-links">
           <Link to="/adminHomePage">Home</Link>
-          <Link to="">Logout</Link>
+          {user ? (
+            <>
+            <button onClick={handleLogout} className="logout-button">
+                Logout
+            </button>
+            </>
+          ) : (
+            <Link to="/adminLogin">Admin Login</Link>
+          )}
         </nav>
       </div>
     </header>
